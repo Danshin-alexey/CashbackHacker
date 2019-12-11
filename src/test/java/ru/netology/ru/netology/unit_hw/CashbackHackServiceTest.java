cc36d9d84404bd829c8c4a075bfdd86568d801d1
Danshin Akexey <ultrabob@inbox.ru>
2019-12-11 20:27:21 +0400

    midified Files

--- src/test/java/ru/netology/ru/netology/unit_hw/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/ru/netology/unit_hw/CashbackHackServiceTest.java
@@ -5,22 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
-
-    @Test
-    void shouldReturn100ifAmountIs900 () {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
-
-        assertEquals(expected, actual);
-    }
     @Test
     void shouldReturn900ifAmountIs1100 () {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1100;
         int actual = cashbackHackService.remain(amount);
         int expected = 900;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturn100ifAmountIs900 () {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
 
         assertEquals(expected, actual);
     }
